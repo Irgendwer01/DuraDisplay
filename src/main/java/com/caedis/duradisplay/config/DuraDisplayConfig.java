8b56b6181d056a35570d2462d8b29dc85664454e
Caedis <Caedis@users.noreply.github.com>
2023-06-05 05:43:30 -0500

    Initial color thresholds

--- src/main/java/com/caedis/duradisplay/config/DuraDisplayConfig.java
+++ src/main/java/com/caedis/duradisplay/config/DuraDisplayConfig.java
@@ -1,12 +1,14 @@
 package com.caedis.duradisplay.config;
 
 import java.io.File;
+import java.util.Arrays;
 
 import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.common.config.Configuration;
 
 import crazypants.enderio.config.Config;
 import gregtech.GT_Mod;
+import net.minecraftforge.common.config.Property;
 
 public class DuraDisplayConfig {
 
@@ -24,9 +26,13 @@ public class DuraDisplayConfig {
     public static boolean Durability_PercentageWhenFull = false;
     public static boolean Charge_PercentageWhenFull = false;
 
+    public static boolean Durability_UseColorThresholds = false;
+    public static double[] Durability_ColorThresholds = new double[] { 25.0, 75.0 };
+
     public static Configuration config = null;
 
     public static void loadConfig() {
+
         if (configLoaded) {
             return;
         }
@@ -46,26 +52,21 @@ public class DuraDisplayConfig {
     }
 
     public static void reloadConfigObject() {
-        Durability_Enable = config.getBoolean(
-            "Durability_Enable",
-            DuraDisplayConfig.CATEGORY_DURABILITY,
-            Durability_Enable,
-            "Enable durability module");
 
-        Durability_HideBar = config.getBoolean(
-            "Durability_HideBar",
-            DuraDisplayConfig.CATEGORY_DURABILITY,
-            Durability_HideBar,
-            "Hide durability bar");
+        Durability_Enable = config
+            .getBoolean("Enable", DuraDisplayConfig.CATEGORY_DURABILITY, Durability_Enable, "Enable durability module");
+
+        Durability_HideBar = config
+            .getBoolean("HideBar", DuraDisplayConfig.CATEGORY_DURABILITY, Durability_HideBar, "Hide durability bar");
 
         Charge_Enable = config
-            .getBoolean("Charge_Enable", DuraDisplayConfig.CATEGORY_CHARGE, Charge_Enable, "Enable charge module");
+            .getBoolean("Enable", DuraDisplayConfig.CATEGORY_CHARGE, Charge_Enable, "Enable charge module");
 
         Charge_HideBar = config
-            .getBoolean("Charge_HideBar", DuraDisplayConfig.CATEGORY_CHARGE, Charge_HideBar, "Hide charge bar");
+            .getBoolean("HideBar", DuraDisplayConfig.CATEGORY_CHARGE, Charge_HideBar, "Hide charge bar");
 
         Durability_PercentageLocation = config.getInt(
-            "Durability_PercentageLocation",
+            "PercentageLocation",
             DuraDisplayConfig.CATEGORY_DURABILITY,
             Durability_PercentageLocation,
             1,
@@ -73,7 +74,7 @@ public class DuraDisplayConfig {
             "Location in item where the durability percentage will be (numpad style)");
 
         Charge_PercentageLocation = config.getInt(
-            "Charge_PercentageLocation",
+            "PercentageLocation",
             DuraDisplayConfig.CATEGORY_CHARGE,
             Charge_PercentageLocation,
             1,
@@ -81,16 +82,39 @@ public class DuraDisplayConfig {
             "Location in item where the charge percentage will be (numpad style)");
 
         Durability_PercentageWhenFull = config.getBoolean(
-            "Durability_PercentageWhenFull",
+            "PercentageWhenFull",
             DuraDisplayConfig.CATEGORY_DURABILITY,
             Durability_PercentageWhenFull,
             "Show durability percentage when item is undamaged/full");
 
         Charge_PercentageWhenFull = config.getBoolean(
-            "Charge_PercentageWhenFull",
+            "PercentageWhenFull",
             DuraDisplayConfig.CATEGORY_CHARGE,
             Charge_PercentageWhenFull,
             "Show charge percentage when item is full");
+
+        Durability_UseColorThresholds = config.getBoolean(
+            "UseColorThresholds",
+            DuraDisplayConfig.CATEGORY_DURABILITY,
+            Durability_UseColorThresholds,
+            "Use the stated color thresholds instead of the default gradient coloring.");
+
+        Property colorThresh = config
+            .get(
+                CATEGORY_DURABILITY,
+                "ColorThresholds",
+                Durability_ColorThresholds,
+                "List of numbers in ascending order from 0-100 that set the thresholds for durability color mapping. "
+                    + "Colors are from Red -> Yellow -> Green with Red being less than or equal to the first value "
+                    + "and Green being greater than or equal to the last value");
+        Durability_ColorThresholds = colorThresh.getDoubleList();
+
+        // clean up whatever the user inputs
+        Durability_ColorThresholds = Arrays.stream(Durability_ColorThresholds)
+            .filter(num -> num >= 0.0 && num <= 100.0)
+            .sorted()
+            .toArray();
+        colorThresh.set(Durability_ColorThresholds);
 
         if (config.hasChanged()) {
             config.save();
