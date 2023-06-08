0e9eb4fec05dde12803ba038cf41bf0eff64afd4
Caedis <Caedis@users.noreply.github.com>
2023-06-08 03:17:03 -0500

    Handle TiCon ammo + unbreaking
    and fix an issue where the durability overlay was not being render.

--- src/main/java/com/caedis/duradisplay/mixins/minecraft/MixinGuiScreen.java
+++ src/main/java/com/caedis/duradisplay/mixins/minecraft/MixinGuiScreen.java
@@ -1,24 +1,26 @@
 package com.caedis.duradisplay.mixins.minecraft;
 
-import com.caedis.duradisplay.render.DurabilityRenderer;
 import net.minecraft.client.gui.GuiScreen;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.caedis.duradisplay.render.DurabilityRenderer;
 
 @Mixin(value = GuiScreen.class)
 public class MixinGuiScreen {
 
     @SuppressWarnings("UnresolvedMixinReference")
     @Inject(method = "drawScreen(IIF)V", at = @At("HEAD"))
-    private void drawScreenStart(CallbackInfo cbi){
+    private void drawScreenStart(CallbackInfo cbi) {
         DurabilityRenderer.ShouldRun = false;
     }
 
     @SuppressWarnings("UnresolvedMixinReference")
     @Inject(method = "drawScreen(IIF)V", at = @At("RETURN"))
-    private void drawScreenEnd(CallbackInfo cbi){
+    private void drawScreenEnd(CallbackInfo cbi) {
         DurabilityRenderer.ShouldRun = true;
     }
 }
